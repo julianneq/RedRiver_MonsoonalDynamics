519de3487796272f76a06a723def758166e63eaa
Julianne Quinn <julianneq@users.noreply.github.com>
2018-08-10 14:15:47 -0400

    Update makeAllFigures.py

--- PaperFigures/makeAllFigures.py
+++ PaperFigures/makeAllFigures.py
@@ -1,10 +1,10 @@
-from makeFigure5 import makeFigure5
-from makeDotPlots import makeDotPlots # makes Figure 6, 7 and 8
+from makeFigure6 import makeFigure6
+from makeDotPlots import makeDotPlots # makes Figures 7abde, 8, 9 and 10
 from makeFigureS1 import makeFigureS1
 
 def makeAllFigures():
 
-	makeFigure5()
+	makeFigure6()
 	makeDotPlots()
 	makeFigureS1()
 
